748fc66a71d90163f1f3b4ef61d73bdbcddc6eb0
Seifalla Ehab <160287704+Seifehab11-8@users.noreply.github.com>
2025-04-19 15:30:57 +0200

    Add files via upload

---
+++ LoginHandler.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.google.gson.Gson;
+
+public class LoginHandler implements HttpHandler{
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String method = exchange.getRequestMethod();
+        if("POST".equals(method)) {
+            BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader(exchange.getRequestBody()));
+            StringBuilder buildRequestBody = new StringBuilder();
+            String line = null;
+            while((line = bufferedReader.readLine()) != null) {
+                buildRequestBody.append(line);
+            }
+            String requestBody = buildRequestBody.toString();
+            User user = new Gson().fromJson(requestBody, User.class);
+            int errorCode = 0;
+            String response = null;
+            /*
+             * TODO: give the code to a function created by mohamed
+             */
+            exchange.getResponseHeaders().add("Content-Type", "application/json");
+            /*
+             * TODO: give the code to a function created by ahmed essam
+             */
+            exchange.sendResponseHeaders(errorCode, 0);
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+        else{
+            throw new UnsupportedOperationException("Unimplemented method 'handle'");
+        }
+    }
+	
+}
